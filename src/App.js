aaee665550366b6de9babb3654c8b2b43a4c560e
Rati <ratigumashvili@gmail.com>
2022-11-01 14:48:59 +0400

    + pages & authorization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 function App() {
-  return <div className="App">hello firebase</div>;
+  const [currentUser, setCurrentUser] = useState(false);
+
+  const RequireUser = ({ children }) => {
+    return currentUser ? children : <Navigate to="/" />;
+  };
+  return (
+    <div className="app">
+      <h1>hello firebase</h1>
+      <Routes>
+        <Route index element={<Login setCurrentUser={setCurrentUser} />} />
+        <Route
+          path="/profile"
+          element={
+            <RequireUser>
+              <Profile />
+            </RequireUser>
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
